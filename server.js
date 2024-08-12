444c840bed4d21bf8690ac7c9a2a70063a95cf0d
Abhishek20110 <71166991+Abhishek20110@users.noreply.github.com>
2024-08-12 19:07:13 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors'; // Import CORS middleware
+import db from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
+import colors from 'colors';
+import inrouter from './routes/insertRoute.js';
+
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+db();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Enable CORS for all routes (you can specify origins if needed)
+app.use(cors({
+    origin: 'http://localhost:3000', // Replace with your React app's URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+// Routes
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/user/action', inrouter);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`.yellow.bold);
+});
